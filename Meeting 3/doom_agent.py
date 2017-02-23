e9e7206f0b48aac25ed6dabb3a4ec281f6a08140
Peter Wittek <peterwittek@users.noreply.github.com>
2017-02-23 23:31:14 +0100

    Doom template script edited

--- Meeting 3/doom_agent.py
+++ Meeting 3/doom_agent.py
@@ -1,6 +1,5 @@
 # Coding group: Alexandre,... 
 import gym
-import gym_pull #to import the environment of doom
-
-gym_pull.pull('github.com/ppaquette/gym-doom')
-env = gym.make('ppaquette/DoomBasic-v0') 
+import ppaquette_gym_doom
+env = gym.make('ppaquette/DoomBasic-v0')
+env.reset()
